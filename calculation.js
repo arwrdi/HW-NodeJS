c386b36025bde336cc8a36ad803a8eb5776c154d
arwrdi <arwinrnrdi@gmail.com>
2023-02-18 15:37:23 +0700

    first init

---
+++ calculation.js
@@ -0,0 +1,6 @@
+exports.luas = function (panjang, lebar) {
+  return panjang * lebar;
+};
+exports.keliling = function (panjang, lebar) {
+  return 2 * (panjang + lebar);
+};
---
+++ soal1.js
@@ -0,0 +1,5 @@
+const calc = require("./calculation");
+console.log(calc.luas(9, 10) + "<<<< luas persegi panjang");
+console.log(calc.luas(9, 9) + "<<<< luas persegi");
+console.log(calc.keliling(9, 10) + "<<<< keliling persegi panjang");
+console.log(calc.keliling(9, 9) + "<<<< keliling persegi");
